fb72bd34ccb4d1d261c7ee375b102c5402127b0c
neutrino0717 <liuenmao@hotmail.com>
2021-09-01 12:48:45 +0800

    init

---
+++ app/src/main/java/com/example/myvpn/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.myvpn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
